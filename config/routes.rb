55f8102cb5e60b7ce2b1ed3ea1f99ce3cbd262f0
Kai Yong <dkang.kky@gmail.com>
2017-05-24 12:13:20 +0800

    authentication implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'sidekiq/web'
 
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
   
   namespace :api do
     namespace :v1 do
@@ -10,5 +9,16 @@ Rails.application.routes.draw do
     end
   end
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  # Protect against timing attacks:
+  # - See https://codahale.com/a-lesson-in-timing-attacks/
+  # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  # - Use & (do not use &&) so that it doesn't short circuit.
+  # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
+  mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
